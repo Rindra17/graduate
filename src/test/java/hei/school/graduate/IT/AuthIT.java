90651875ceb323907fab3c0460cf84edf51a8929
Rindraniaina Rasolofondrasoa <rindraniainarasolofondrasoa17@gmail.com>
2026-08-14 18:00:55 +0300

    feat: expose jwt token in login response

--- src/test/java/hei/school/graduate/IT/AuthIT.java
+++ src/test/java/hei/school/graduate/IT/AuthIT.java
@@ -160,7 +160,8 @@ class AuthIT extends FacadeIT {
 
     assertEquals(ACCEPTED, response.getStatusCode());
     assertNotNull(response.getBody());
-    assertEquals(email, response.getBody().get("email"));
+    assertEquals(email, userOf(response.getBody()).get("email"));
+    assertNotNull(response.getBody().get("token"));
     assertTrue(response.getHeaders().containsKey("Set-Cookie"));
   }
 
@@ -177,7 +178,7 @@ class AuthIT extends FacadeIT {
 
     assertEquals(ACCEPTED, response.getStatusCode());
     assertNotNull(response.getBody());
-    assertEquals(true, response.getBody().get("mustChangePassword"));
+    assertEquals(true, userOf(response.getBody()).get("mustChangePassword"));
   }
 
   @Test
@@ -275,7 +276,7 @@ class AuthIT extends FacadeIT {
 
     assertEquals(ACCEPTED, loginResponse.getStatusCode());
     assertNotNull(loginResponse.getBody());
-    assertEquals(false, loginResponse.getBody().get("mustChangePassword"));
+    assertEquals(false, userOf(loginResponse.getBody()).get("mustChangePassword"));
   }
 
   @Test
